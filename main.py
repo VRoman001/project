c3cbe0e4141b4b1465cce9697f2e0bea2698f79c
VRoman001 <143933827+VRoman001@users.noreply.github.com>
2024-12-29 13:28:57 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,37 @@
-from direct.showbase.ShowBase import ShowBase
-from mapmanager import Mapmanager
+from flask import Flask,render_template
+import sqlite3
+import random
+app = Flask(__name__)
 
 
-class Game(ShowBase):
-   def __init__(self):
-       ShowBase.__init__(self)
-       self.land = Mapmanager() # створюємо о;lManager
-       self.land.loadMap() # завантажуємо карту
-       base.camLens.setFov(90)
+def connect_db():
+    conn = sqlite3.connect('data.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM test")
+    rows = cursor.fetchall()
+
+    result = []
+    for r in rows:
+        dictionary = {
+            'name': r[0]
+        }
+        result.append(dictionary)
+    conn.close()
+    return result
+@app.route('/')
+def home():
+    return render_template('main.html')
+
+@app.route('/paig')
+def paig1():
+    return render_template('paig.html')
+
+@app.route('/paig2')
+def paig2():
+    n = connect_db()
+    return render_template('paig2.html', recepts = n)
+
+if __name__ == '__main__':
+    app.run()
 
 
-game = Game()
-game.run()
