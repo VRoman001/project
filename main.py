5e6b16892a379720204f9aaac233504ac83846d8
VRoman001 <143933827+VRoman001@users.noreply.github.com>
2025-01-19 13:17:44 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect
 import sqlite3
 import random
 app = Flask(__name__)
@@ -7,13 +7,15 @@ app = Flask(__name__)
 def connect_db():
     conn = sqlite3.connect('data.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT name FROM test")
+    cursor.execute("SELECT Recepts, Ingredients, Instruction FROM test")
     rows = cursor.fetchall()
 
     result = []
     for r in rows:
         dictionary = {
-            'name': r[0]
+            'Recepts': r[0],
+            'Ingredients': r[1],
+            'Instuction': r[2]
         }
         result.append(dictionary)
     conn.close()
@@ -31,7 +33,18 @@ def paig2():
     n = connect_db()
     return render_template('paig2.html', recepts = n)
 
+# Маршрут для обробки даних форми
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    recepts = request.form['recipeName']
+    ingredients = request.form['ingredients']
+    instructions = request.form['instructions']
+    with sqlite3.connect('data.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO test (Recepts, Ingredients, Instruction) VALUES (?, ?, ?)', (recepts, ingredients, instructions))
+        conn.commit()
+    
+    return redirect("/")
+
 if __name__ == '__main__':
     app.run()
-
-
